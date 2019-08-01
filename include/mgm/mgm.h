38378267ad63fe18cc43bd72b991458e922d54aa
methylDragon <methylDragon@gmail.com>
2019-08-01 21:41:04 +0800

    Create library header

---
+++ include/mgm/mgm.h
@@ -0,0 +1,25 @@
+#ifndef LIBMGM_
+#define LIBMGM_
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include "math.h"
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <cmath>
+#include "assert.h"
+
+#include "smartparameter.h"
+#include "img_tools.h"
+#include "mgm_multiscale.h"
+#include "stereo_utils.h"
+
+//// a structure to wrap images 
+#include "img_interp.h"
+#include "point.h"
+
+#endif
